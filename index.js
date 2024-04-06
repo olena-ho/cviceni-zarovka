e63c9519aa984cfc6b23284e2dac17d60bc6d858
Olena Horovenko <elena.gorovenko@gmail.com>
2024-04-06 16:00:17 +0200

    classList.add or toggle done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('funguju!');
+document.addEventListener('click', function(event) {
+  const bulb = document.querySelector('.bulb') //with 'keydown' - it would work with pressing keyboard keys, if 'click' - than with clicking the mose or finger
+
+/*
+  bulb.classList.add('bulb--on');// this just adds the class - so it turns on the bulb forever */
+
+  bulb.classList.toggle('bulb--on'); //this enables the bulb to switch on and off with every next time a key is pressed/or mouse clicked
+});
+
